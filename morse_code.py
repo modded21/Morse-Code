d04d71d17dd0da47b8721abc38ce9289db37186a
Jaelin Tyen Fernando <jaelinfernando123@gmail.com>
2024-04-29 16:07:31 -0400

    Update morse_code.py
    Changed the morse list and the functions where the audio would be played depending on the user's input.

--- morse_code.py
+++ morse_code.py
@@ -1,151 +1,57 @@
-# Programming Exercise 8-4
-# This program will not only get Python to convert a user's string
-# as Morse Code, but it will also read the Morse Code aloud.
-# NOTE: I am writing this on macOS, however I used import winsound
-# so that this program will be accessible on Windows PC's.
+# This was part of an exercise we did in class, this program is only capable of running on Windows, so I apologize to my fellow macOS users.
 import winsound
 import time
+
+morse_dict = {
+    ' ': ' ', ',': '--..--', '.': '.-.-.-', '?': '..--..',
+    '0': '-----', '1': '.----', '2': '..---', '3': '...--',
+    '4': '....-', '5': '.....', '6': '-....', '7': '--...',
+    '8': '---..', '9': '----.', 'A': '.-', 'B': '-...',
+    'C': '-.-.', 'D': '-..', 'E': '.', 'F': '..-.',
+    'G': '--.', 'H': '....', 'I': '..', 'J': '.---',
+    'K': '-.-', 'L': '.-..', 'M': '--', 'N': '-.',
+    'O': '---', 'P': '.--.', 'Q': '--.-', 'R': '.-.',
+    'S': '...', 'T': '-', 'U': '..-', 'V': '...-',
+    'W': '.--', 'X': '-..-', 'Y': '-.--', 'Z': '--..'
+}
+
 def main():
-    # Local variables
-    morse_string = ''
-    index = 0
-    # List of Morse codes
-    morse_list = [' ', '--..--', '.-.-.-','..--..','-----',
-                  '.----', '..---', '...--', '....-',
-                  '.....', '-....', '--...', '---..',
-                  '----.', '.-', '-...', '-.-.', '-..',
-                  '.', '..-.', '--.', '....', '..', '.---',
-                  '-.-', '.-..', '--', '-.', '---', '.--.',
-                  '--.-', '.-.', '...', '-', '..-', '...-',
-                  '.--', '-..-', '-.-', '--..']
     # Get the string as input from the user.
     morse_string = input('Enter the string to be ' \
                          'converted to Morse code: ')
     # Step through the string, determine each
-    # character's Morse code index in the list,
-    # and display the Morse code for the character.
-    for ch in morse_string:
-        # Convert the character to uppercase.
-        ch = ch.upper()
-
-    # Determine the index from the list.
-        if ch == ' ':
-            index = 0
-        elif ch == ',':
-            index = 1
-        elif ch == '.':
-            index = 2
-        elif ch == '?':
-            index = 3
-        elif ch == '0':
-            index = 4
-        elif ch == '1':
-            index = 5
-        elif ch == '2':
-            index = 6
-        elif ch == '3':
-            index = 7
-        elif ch == '4':
-            index = 8
-        elif ch == '5':
-            index = 9
-        elif ch == '6':
-            index = 10
-        elif ch == '7':
-            index = 11
-        elif ch == '8':
-            index = 12
-        elif ch == '9':
-            index = 13
-        elif ch == 'A':
-            index = 14
-        elif ch == 'B':
-            index = 15
-        elif ch == 'C':
-            index = 16
-        elif ch == 'D':
-            index = 17
-        elif ch == 'E':
-            index = 18
-        elif ch == 'F':
-            index = 19
-        elif ch == 'G':
-            index = 20
-        elif ch == 'H':
-            index = 21
-        elif ch == 'I':
-            index = 22
-        elif ch == 'J':
-            index = 23
-        elif ch == 'K':
-            index = 24
-        elif ch == 'L':
-            index = 25
-        elif ch == 'M':
-            index = 26
-        elif ch == 'N':
-            index = 27
-        elif ch == 'O':
-            index = 28
-        elif ch == 'P':
-            index = 29
-        elif ch == 'Q':
-            index = 30
-        elif ch == 'R':
-            index = 31
-        elif ch == 'S':
-            index = 32
-        elif ch == 'T':
-            index = 33
-        elif ch == 'U':
-            index = 34
-        elif ch == 'V':
-            index = 35
-        elif ch == 'W':
-            index = 36
-        elif ch == 'X':
-            index = 37
-        elif ch == 'Y':
-            index = 38
-        elif ch == 'Z':
-            index = 39
-
-unit = 100  # ms
-frequency = 3000  # Hz
+    # character's Morse code and play the corresponding sound.
+    for ch in morse_string.upper():
+        if ch in morse_dict:
+            txt2morse(morse_dict[ch])
+        else:
+            print(f"Character '{ch}' is not supported in Morse code.")
 
 
 def play_dot():
-    winsound.Beep(frequency, 1*unit)
+    winsound.Beep(1000, 100)  # Frequency of 1000 Hz for dot, duration of 100 ms
 
 
 def play_dash():
-    winsound.Beep(frequency, 3*unit)
+    winsound.Beep(1000, 300)  # Frequency of 1000 Hz for dash, duration of 300 ms
 
 
 def play_char(ch):
-    if ch == ' ':
-        time.sleep(7*unit/1000)
-        return '<space>'
-
-    morse_string = morse_list[ch]
-    for d in morse_value:
+    for d in ch:
         if d == '.':
             play_dot()
-        else:
+        elif d == '-':
             play_dash()
-        time.sleep(1*unit/1000)
+        else:
+            time.sleep(0.7)  # Space between words
+    return ch
 
-    return morse_value
 
-
-def txt2morse(txt):
-    for ch in txt.upper():
+def txt2morse(morse_code):
+    for ch in morse_code:
         print(play_char(ch))
+    time.sleep(0.3)  # Space between characters
 
-
-if __name__ == "__main__":
-    txt2morse('SOS SOS')
-    txt2morse('hello world')
 
 # Call the main function.
 main()
